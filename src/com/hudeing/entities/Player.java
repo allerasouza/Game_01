c7c1a974c5aaf809891e253efd7eea3e1d1067b5
Aller Augusto de Souza <aller.souza@samsung.com>
2020-01-31 16:29:59 -0300

    Change renderization and loop

--- src/com/hudeing/entities/Player.java
+++ src/com/hudeing/entities/Player.java
@@ -1,28 +1,74 @@
 package com.hudeing.entities;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+
+import com.hudeing.main.Game;
 
 public class Player extends Entity{
 	
 	public boolean right, up, left, down;
-	public double speed = 0.7;
+	public int right_dir = 0, left_dir = 1;
+	public int dir = right_dir;
+	public double speed = 1.4;
+	
+	private int frames = 0, maxFrames = 5, index = 0, maxIndex = 3;
+	private boolean moved = false;
+	private BufferedImage[] rightPlayer;
+	private BufferedImage[] leftPlayer;
 
 	public Player(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
+		rightPlayer = new BufferedImage[4];
+		leftPlayer = new BufferedImage[4];
+		
+		for(int i = 0; i < rightPlayer.length; i++) {
+			rightPlayer[i] = Game.spritesheet.getSprite(32 +(16*i), 0, 16, 16);
+		}
+		for(int i = 0; i < leftPlayer.length; i++) {
+			leftPlayer[i] = Game.spritesheet.getSprite(32 +(16*i), 16, 16, 16);
+		}
+		
+		
 	}
 	
 	public void tick() {
+		moved = false;
 		if(right) {
+			moved = true;
 			x+=speed; //this.setX(getX() + speed);
+			dir = right_dir;
 		} else if(left) {
+			moved = true;
 			x-=speed; //this.setX(getX() - speed);
+			dir = left_dir;
 		}
 		
 		if(up) {
+			moved = true;
 			y-=speed; //this.setY(getY() + speed);
 		} else if(down) {
+			moved = true;
 			y+=speed; //this.setY(getY() - speed);
+		}
+		
+		if(moved) {
+			frames++;
+			if(frames == maxFrames) {
+				frames = 0;
+				index++;
+				if(index > maxIndex) {
+					index = 0;
+				}
+			}
 		}
 	}
 
+	public void render(Graphics g) {
+		if(dir == right_dir) {
+			g.drawImage(rightPlayer[index], this.getX(), this.getY(), null);
+		} else if(dir == left_dir) {
+			g.drawImage(leftPlayer[index], this.getX(), this.getY(), null);
+		}
+	}
 }
--- src/com/hudeing/main/Game.java
+++ src/com/hudeing/main/Game.java
@@ -31,7 +31,7 @@ public class Game extends Canvas implements Runnable, KeyListener{
 	private final int SCALE = 3;
 	private BufferedImage image;
 	public List<Entity> entities;
-	public Spritesheet spritesheet;
+	public static Spritesheet spritesheet;
 	private Player player;
 		
 	public Game() {

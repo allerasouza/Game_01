402bf22136f9ddc482eae17644a0fac7d999b43f
Aller Augusto de Souza <aller.souza@samsung.com>
2020-02-02 16:18:29 -0300

    Add animation to enemies

--- src/com/hudeing/entities/Enemy.java
+++ src/com/hudeing/entities/Enemy.java
@@ -2,10 +2,28 @@ package com.hudeing.entities;
 
 import java.awt.image.BufferedImage;
 
+import com.hudeing.main.Game;
+
 public class Enemy extends Entity{
+	
+	private double speed = 1;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
+	}
+	
+	public void tick() {
+		if((int)x < Game.player.getX()) {
+			x += speed;
+		} else if((int)x > Game.player.getX()) {
+			x -= speed;
+		}
+		
+		if((int)y < Game.player.getY()) {
+			y += speed;
+		} else if((int)y > Game.player.getY()) {
+			y -= speed;
+		}
 	}
 
 }

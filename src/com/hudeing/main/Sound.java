f946302e000dd74ff69986121441b911e8972f87
Aller Augusto de Souza <aller.souza@samsung.com>
2020-02-24 11:51:36 -0300

    Update sound class

--- src/com/hudeing/main/Sound.java
+++ src/com/hudeing/main/Sound.java
@@ -1,43 +1,62 @@
 package com.hudeing.main;
 
-import java.applet.Applet;
-import java.applet.AudioClip;
-
-@SuppressWarnings("deprecation")
+import java.io.*;
+import javax.sound.sampled.*;
 public class Sound {
-	private AudioClip clip;
-	public static final Sound musicBackground = new Sound("/music.wav");
-	public static final Sound hurtEffect = new Sound("/hurt.wav");
 	
-	private Sound(String name) {
-		try {
-			clip = Applet.newAudioClip(Sound.class.getResource(name));
-		} catch (Exception e) {
-			// TODO: handle exception
+	public static class Clips {
+		public Clip[] clips;
+		private int p;
+		private int count;
+		
+		public Clips(byte[] buffer, int count) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+			if(buffer == null)
+				return;
+			clips = new Clip[count];
+			this.count = count;
+			for(int i = 0; i < count; i++) {
+				clips[i] = AudioSystem.getClip();
+				clips[i].open(AudioSystem.getAudioInputStream(new ByteArrayInputStream(buffer)));
+			}
+		}
+		
+		public void play() {
+			if(clips == null) return;
+			clips[p].stop();
+			clips[p].setFramePosition(0);
+			clips[p].start();
+			p++;
+			if(p >= count) p = 0;
+		}
+		
+		public void loop() {
+			if(clips == null) return;
+			clips[p].loop(300);
 		}
 	}
 	
-	public void play() {
-		try {
-			new Thread() {
-				public void run() {
-					clip.play();
-				}
-			}.start();
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
+	public static Clips musicBackground = load("/music.wav", 1);
+	public static Clips hurtEffect = load("/hurt.wav", 1);
 	
-	public void loop() {
+	private static Clips load(String name, int count) {
 		try {
-			new Thread() {
-				public void run() {
-					clip.loop();
-				}
-			}.start();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			DataInputStream dis = new DataInputStream(Sound.class.getResourceAsStream(name));
+			
+			byte[] buffer = new byte[1024];
+			int read = 0;
+			while((read = dis.read(buffer)) >= 0) {
+				baos.write(buffer, 0 , read);
+			}
+			dis.close();
+			byte[] data = baos.toByteArray();
+			return new Clips(data, count);
 		} catch (Exception e) {
-			// TODO: handle exception
+			try {
+				return new Clips(null, 0);
+			} catch (Exception e2) {
+				return null;
+			}
 		}
 	}
 }

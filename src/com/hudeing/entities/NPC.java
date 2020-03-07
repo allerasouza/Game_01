0ceb18df950cbafda0b6eb9a9e47ab057ba7e801
Aller Augusto de Souza <aller.souza@samsung.com>
2020-03-07 12:36:00 -0300

    Add dialog animation

--- src/com/hudeing/entities/NPC.java
+++ src/com/hudeing/entities/NPC.java
@@ -12,14 +12,18 @@ public class NPC extends Entity{
 	public String[] frases = new String[5];
 	public boolean showMessage = false;
 	public boolean show = false;
+	public int curIndexMsg = 0;
+	public int fraseIndex = 0;
+	public int time = 0;
+	public int maxTime = 5;
 
 	public NPC(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
-		frases[0] = "Olá!";
-		frases[1] = "X1";
-		frases[2] = "X2";
-		frases[3] = "X3";
-		frases[4] = "X4";
+		frases[0] = "Olá! Seja muito bem-vindo ao jogo :)";
+		frases[1] = "Destrua os inimigos!";
+		frases[2] = "XXXXXXXXXXXXXX2";
+		frases[3] = "XXXXXXXXXXXXXXXXXXXXXXXXX3";
+		frases[4] = "XXXXXX4";
 		
 				
 	}
@@ -41,6 +45,21 @@ public class NPC extends Entity{
 			//showMessage = false;
 			show = false;
 		}
+		
+		if(showMessage) {
+			this.time++;
+			if(this.time >= this.maxTime) {
+				this.time = 0;
+				if(curIndexMsg < frases[fraseIndex].length()) {
+					curIndexMsg++;
+				} else {
+					if(fraseIndex < frases.length -1) {
+						fraseIndex++;
+						curIndexMsg = 0;
+					}
+				}
+			}
+		}
 	}
 	
 	public void render(Graphics g) {
@@ -51,9 +70,9 @@ public class NPC extends Entity{
 			g.fillRect(9, 9, Game.WIDTH - 18, Game.HEIGHT - 18);
 			g.setColor(Color.BLUE);
 			g.fillRect(10, 10, Game.WIDTH - 20, Game.HEIGHT - 20);
-			g.setFont(new Font("Arial", Font.BOLD, 9));
+			g.setFont(new Font("Arial", Font.BOLD, 7));
 			g.setColor(Color.WHITE);
-			g.drawString(frases[0], (int)x, (int)y);
+			g.drawString(frases[fraseIndex].substring(0, curIndexMsg), (int)x, (int)y);
 			
 			g.drawString(">Pressione Enter para Fechar<", (int)x + 10, (int)y + 13);
 		}
